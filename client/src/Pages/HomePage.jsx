529ea4ac79a5c8a28a7eace1dc60b856fc3fd31a
M-ADARSHCHARY <adarshchary.05@gmail.com>
2025-08-29 13:40:19 +0530

    font Changed

--- client/src/Pages/HomePage.jsx
+++ client/src/Pages/HomePage.jsx
@@ -7,11 +7,15 @@ const HomePage = () => {
   const {selectedUser} = useChatStore();
   return (
     <div className="h-85vh bg-transparent">
-            <div className="w-[100%]  h-fit rounded-lg bg-base-100 grid grid-cols-[1fr_3fr] md:grid-cols-[1fr_3fr]">
+            <div className="w-[100%]  h-fit rounded-lg bg-base-100 grid grid-cols-[1fr_3fr] md:grid-cols-[1fr_3fr] relative">
                 <div className="h-[90vh] rounded-lg overflow-hidden ">
                     <SideBar/>
                  </div>     
-                  {!selectedUser ? (<NoChatSelected/>) : (<ChatBox/>)}
+                  {!selectedUser ? (
+                    <div className="h-fit w-fit relative top-[20%] md:top-[30%] lg:top-[30%] mx-auto">
+                       <NoChatSelected/>
+                    </div>
+                    ) : (<ChatBox/>)}
             </div>
     </div>
   )
